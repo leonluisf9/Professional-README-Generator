4a57dc0218c2e7895a798684a43558c908840ec2
Maverick <leonluisf9@outlook.com>
2025-02-02 13:01:45 -0600

    fix badge license bug 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 import { generateMarkdown } from './utils/generateMarkdown.js';
 
-const licenses = ['None', 'MIT License', 'LGPL 3.0', 'MPL 2.0', 'AGPL 3.0', 'Unlicense', 'Apache 2.0','GPL 3.0'];
+const licenses = ['None', 'MIT_License', 'LGPL_3.0', 'MPL_2.0', 'AGPL_3.0', 'Unlicense', 'Apache_2.0','GPL_3.0'];
 
 // TODO: Create an array of questions for user input
 const questions = [
